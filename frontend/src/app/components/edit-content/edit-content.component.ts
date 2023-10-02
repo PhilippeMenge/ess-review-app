6e78a849abf0acdc9322de1a9ff8256ea3a07cf5
PhilippeMenge <philippemengealima@gmail.com>
2023-10-02 17:12:20 -0300

    feat: edit-content frontend

--- frontend/src/app/components/edit-content/edit-content.component.ts
+++ frontend/src/app/components/edit-content/edit-content.component.ts
@@ -1,25 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'app/services/auth.service';
+import { User } from '../../models/userModel';
 import { ContentModel } from '../../models/content';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-edit-content',
   templateUrl: './edit-content.component.html',
   styleUrls: ['./edit-content.component.scss']
 })
-export class EditContentComponent {
-    contentdto = new ContentModel();
-
-    constructor(private authservice: AuthService){
-      
-    }
+export class EditContentComponent implements OnInit{
+  username: string | null = null;
+  user: User | undefined;
+  contentDto = new ContentModel();
+  constructor(private router: Router, private authService: AuthService){}
+  ngOnInit() {
+    this.authService.getUserData().subscribe({
+      next: (user) =>{
+        this.username = user.username;
+        this.user = user;
+      },
+      error: (error) =>{
+        console.error("Erro ao obter o usuário", error);
+      }
+    })
+  }
 
     editContent(contentdto: ContentModel){
-      return this.authservice.editContent(contentdto).subscribe()
+      return this.authService.editContent(contentdto).subscribe()
        
     }
     removeContent(contentdto: ContentModel){
-      return this.authservice.removeContent(contentdto).subscribe()
+      return this.authService.removeContent(contentdto).subscribe()
+    }
+    redirectToProfile(event: Event){
+      event.preventDefault();
+      this.router.navigate(['profile']);
+    }
+    
+    redirectToHome(event: Event){
+      event.preventDefault();
+      this.router.navigate(['home', ]);
+    }
+  
+    redirectToFilms(event: Event){
+      event.preventDefault();
+      this.router.navigate(['profile']);
+    }
+    logout() {
+      localStorage.removeItem('jwtToken');
+  
+      this.router.navigate(['home']);
     }
 }
 
